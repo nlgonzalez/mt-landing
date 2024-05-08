2fd18d8bd2b2412ce81faf7f7afd569950bf5b5f
Nicolas Gonzalez <nicolas.gonzalez@carian.healthcare>
2024-05-08 05:09:14 -0700

    feat: fixed custom components

--- components/plasmic-init.ts
+++ components/plasmic-init.ts
@@ -1,72 +1,5 @@
 import { registerComponent } from "@plasmicapp/react-web/lib/host";
-
-import React from "react";
-
-interface MenuItemProps {
-  label: string;
-  onClick: () => void;
-  isSelected?: boolean;
-}
-
-const MenuItem: React.FC<MenuItemProps> = ({ label, onClick, isSelected }) => {
-  const activeClass = isSelected ? 'menu-item-active' : '';
-  return (
-    <div className={`menu-item ${activeClass}`} onClick={onClick}>
-      {label}
-    </div>
-  );
-}
-
-
-interface MenuProps {
-  mode: 'horizontal' | 'vertical';  // Here you can add more modes as needed
-  theme: 'light' | 'dark';          // Extend this to include other themes
-  selectedKeys?: string[];
-  defaultSelectedKeys: string[];
-  children: React.ReactNode;
-}
-
-const Menu: React.FC<MenuProps> = ({
-  mode,
-  theme,
-  selectedKeys,
-  defaultSelectedKeys,
-  children
-}) => {
-  const [activeKeys, setActiveKeys] = useState<string[]>(defaultSelectedKeys);
-
-  useEffect(() => {
-    if (selectedKeys) {
-      setActiveKeys(selectedKeys);
-    }
-  }, [selectedKeys]);
-
-  const handleItemClick = (key: string) => {
-    if (!selectedKeys) { // Only update if not controlled externally
-      setActiveKeys([key]);
-    }
-    console.log(`Item with key ${key} clicked`);
-  };
-
-  const renderMenuItems = () => {
-    return React.Children.map(children, child => {
-      if (React.isValidElement(child)) {
-        const isSelected = activeKeys.includes(child.key as string);
-        return React.cloneElement(child, {
-          onClick: () => handleItemClick(child.key as string),
-          isSelected
-        });
-      }
-      return child;
-    });
-  };
-
-  return (
-    <nav className={`menu ${mode} ${theme}`}>
-      {renderMenuItems()}
-    </nav>
-  );
-}
+import {Menu, MenuItem} from "./Menu"
 
 registerComponent(MenuItem, {
   name: 'MenuItem',
@@ -117,4 +50,3 @@ registerComponent(Menu, {
   }
 });
 
-export { Menu, MenuItem };
